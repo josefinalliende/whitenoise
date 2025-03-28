da0d9cc3eece493b2ee7e5025d84de001a3fb7ae
josefinalliende <moalliende@uc.cl>
2025-03-28 11:42:58 -0300

    fix(storybook): use bun in deploy action

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/sveltekit";
 
 const config: StorybookConfig = {
-    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
+    stories: ["../src/**/*.stories.@(ts|svelte)"],
     addons: ["@storybook/addon-essentials", "@storybook/addon-svelte-csf"],
     framework: {
         name: "@storybook/sveltekit",
